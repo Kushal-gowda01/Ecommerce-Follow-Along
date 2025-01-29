d752107595fad4f6cf1daff29f266df880339bfb
Kushal-gowda01 <kushalgowda8586@gmail.com>
2025-01-29 13:58:25 +0530

    Update App.jsx

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -2,6 +2,8 @@
 import './App.css'
 import {Route, Routes} from 'react-router-dom'
 import Login from './component/Login'
+import Signup from './component/signup'
+import { Home } from './page/home'
 
 function App() {
   
@@ -10,6 +12,8 @@ function App() {
     <>
       <Routes>
         <Route path ="/login" element = {<Login />} />
+        <Route path="/signup" element = {<Signup/>}/>
+        <Route path="/home" element = {<Home/>}/>
       </Routes>
     </>
   )
