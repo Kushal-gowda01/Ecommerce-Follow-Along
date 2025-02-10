2df60417ccffb04e6516737493ef28df0b9fbc96
kushal gowda P <kushalgowda8586@gmail.com>
2025-02-10 15:53:02 +0530

    added

--- FrontEnd/src/component/Product.jsx
+++ FrontEnd/src/component/Product.jsx
@@ -1,10 +1,30 @@
-import React from "react";
+import React,{useEffect,useState} from 'react';
 import "../component/product.css"
+import PropTypes from 'prop-types';
 
 export const Product  = ({image,name,price,description})=>{
+
+    useEffect(()=>{
+        document.body.style.backgroundColor='azure'
+      })
+    
+      
+    const [imgIndex,setImgIndex] = useState(0);
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setImgIndex((prev) => {
+                console.log(prev + 1);
+                return (prev + 1)%(product.image.length-1) ;
+            });
+        }, 2000);
+    
+        return () => clearInterval(interval); // Cleanup when unmounting
+    }, [imgIndex]);
+
     return(
         <div className="bg-white rounded-lg shadow-lg p-4">
-            <img src={image} alt="product" className="w-full h-40 object-cover"/>
+            <img src={image} alt="product" className="w-80 h-100 object-cover"/>
             <h1 className="text-lg font-bold text-neutral-900">{name}</h1>
             <p className="text-neutral-500">{price}</p>
             <p className="text-neutral-700">{description}</p>
--- FrontEnd/src/page/home.jsx
+++ FrontEnd/src/page/home.jsx
@@ -1,27 +1,53 @@
-import React from "react";
+import React,{useState} from 'react';
 import { Product } from "../component/Product";
+import axios from 'axios'
 
-const productdetails = [
-    {
-        image: "https://images.samsung.com/is/image/samsung/assets/in/explore/brand/5-best-android-mobile-phones-2022-in-india/banner-mobile-720x761-080422.jpg?$720_N_JPG$",
-        name:"Product1",
-        price: "$100",
-        description: "This is a product1"
-    },
-    {
-        image: "https://images.samsung.com/is/image/samsung/assets/in/explore/brand/5-best-android-mobile-phones-2022-in-india/banner-mobile-720x761-080422.jpg?$720_N_JPG$",
-        name:"Product2",
-        price: "$100",
-        description: "This is a product2"
-    },
-    {
-        image: "https://images.samsung.com/is/image/samsung/assets/in/explore/brand/5-best-android-mobile-phones-2022-in-india/banner-mobile-720x761-080422.jpg?$720_N_JPG$",
-        name:"Product3",
-        price: "$100",
-        description: "This is a product3"
-    }
-]
+
+// const productdetails = [
+//     {
+//         image: "https://images.samsung.com/is/image/samsung/assets/in/explore/brand/5-best-android-mobile-phones-2022-in-india/banner-mobile-720x761-080422.jpg?$720_N_JPG$",
+//         name:"Product1",
+//         price: "$100",
+//         description: "This is a product1"
+//     },
+//     {
+//         image: "https://images.samsung.com/is/image/samsung/assets/in/explore/brand/5-best-android-mobile-phones-2022-in-india/banner-mobile-720x761-080422.jpg?$720_N_JPG$",
+//         name:"Product2",
+//         price: "$100",
+//         description: "This is a product2"
+//     },
+//     {
+//         image: "https://images.samsung.com/is/image/samsung/assets/in/explore/brand/5-best-android-mobile-phones-2022-in-india/banner-mobile-720x761-080422.jpg?$720_N_JPG$",
+//         name:"Product3",
+//         price: "$100",
+//         description: "This is a product3"
+//     }
+// ]
 export const Home =()=>{
+
+    const [productDetails,setProductDetails]=useState([])
+    const [loading,setLoading]=useState(true);
+    const [error,setError]=useState("")
+
+    useEffect(()=>{
+        async ()=>{
+            await axios.fetch("http://localhost:3000/product/get-products")
+        .then((res)=>{
+            if (!res.ok){
+                throw new Error(`HTTP Error! status:${res.status}`)
+            }
+            return res.json()
+        }).then((data)=>{
+            setProductDetails(data.Products);
+            setLoading(false);
+            
+        }).catch((err)=>{
+            console.error(err)
+            setError(err)
+        })
+        }
+        
+    })
     return(
         <>
         <div className="p-4 display flex justify-center items-center">
@@ -29,7 +55,7 @@ export const Home =()=>{
         </div>
         <div className="w-full min-h-screeen p-4 display flex justify-center items-center mt-20">
         <div className="grid grid-cols-1 gap-4 md:grid-cols-3">
-            {productdetails.map((product,index)=>{
+            {productDetails.map((product,index)=>{
                 return(
                     <>
                     <Product {...product}/>
