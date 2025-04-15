8e8bd3e79c2b4e0f104b2df7ef3be549d0fa4fb4
Kushal-gowda01 <kushalgowda8586@gmail.com>
2025-04-15 09:20:42 +0530

    Update App.jsx

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -1,16 +1,14 @@
 import React from 'react'
-import { BrowserRouter,Routes,Route } from 'react-router-dom';
-import {LoginPage,SignupPage,Home,CreateProduct, MyProducts, Cart, ProductDetails, Profile, CreateAddress, SelectAddress, OrderConfirmation, MyOrdersPage} from "./Routes";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { LoginPage, SignupPage, Home, CreateProduct, MyProducts, Cart, ProductDetails, Profile, CreateAddress, SelectAddress, OrderConfirmation, MyOrdersPage } from "./Routes";
 import "./App.css";
 
-
 const App = () => {
-  
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<Home/>}/>
-        <Route path='/login' element={<LoginPage/>}/>
+        <Route path='/' element={<Home />} />
+        <Route path='/login' element={<LoginPage />} />
         <Route path="/signup" element={<SignupPage />} />
         <Route path="/create-product" element={<CreateProduct />} />
         <Route path="/create-product/:id" element={<CreateProduct />} />
@@ -22,11 +20,9 @@ const App = () => {
         <Route path="/select-address" element={<SelectAddress />} />
         <Route path="/order-confirmation" element={<OrderConfirmation />} />
         <Route path="/myorders" element={<MyOrdersPage />} />
-        <Route path ="/login" element = {<Login />} />
-
       </Routes>
     </BrowserRouter>
-  )
-}
+  );
+};
 
 export default App;
